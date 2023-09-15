3cf86e2c82c519d8bb6c4e91a2f1b3d2fe384aa6
Alyse-R <alyse.ridge@gov.ab.ca>
2023-09-15 17:53:55 -0600

    Make the basic page outline

---
+++ pages/timeline.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_timeline_page():
+    st.title("Timeline Page")
+    st.write("This is the Timeline page content.")
---
+++ pages/trivia.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_trivia_page():
+    st.title("Trivia Page")
+    st.write("This is the Trivia page content.")
