946c78346dbc0926e6c5d0f8360dde10c0ed2d22
Alyse-R <alyse.ridge@gov.ab.ca>
2023-09-16 18:21:24 -0600

    Add welcome page and connect it to trivia

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 from pages.trivia import show_trivia_page
-from pages.timeline import show_timeline_page
 
 def main():
-    st.sidebar.title("Navigation")
-    selected_page = st.sidebar.radio("Go to", ("Trivia", "Timeline"))
+    # Title with increased font size and bold styling
+    st.markdown(
+    "<h1 style='text-align: center; color: white; font-size: 3em; font-weight: bold; " \
+    "text-shadow: -2px -2px 0 #8a2be2, 2px -2px 0 #8a2be2, -2px 2px 0 #8a2be2, 2px 2px 0 #8a2be2;'>Welcome to</h1>",
+    unsafe_allow_html=True
+    )
 
-    if selected_page == "Trivia":
+    # Title with increased font size and bold styling
+    st.markdown(
+    "<h2 style='text-align: center; color: white; font-size: 6em; " \
+    "text-shadow: -2px -2px 0 #8a2be2, 2px -2px 0 #8a2be2, -2px 2px 0 #8a2be2, 2px 2px 0 #8a2be2;'>" \
+    "Quizzing the Wage Divide: A Gender Gap Guessing Game</h2>",
+    unsafe_allow_html=True
+    )
+    if st.button("Start Trivia"):
         show_trivia_page()
-    elif selected_page == "Timeline":
-        show_timeline_page()
 
 if __name__ == "__main__":
     main()
--- pages/timeline.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show_timeline_page():
-    st.title("Timeline Page")
-    st.write("This is the Timeline page content.")
